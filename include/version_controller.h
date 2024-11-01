660e63904a5232e381a8565664acb115de3f071c
Cory Dyer <cjdyer0@gmail.com>
2024-11-01 20:01:01 +1000

    fix typo

--- include/version_controller.h
+++ include/version_controller.h
@@ -12,7 +12,7 @@ class VersionController
 public:
     VersionController();
 
-    // Commiting
+    // Committing
     void create(const std::string &file_path, const std::string &message);
     void restore(size_t index, const std::string &file_path);
     void diff(size_t base_index, size_t alt_index);
